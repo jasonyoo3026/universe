ef76f9ee60a547f2fae26348e51d01fc4754d4b7
Jason (Jung Hyun) Yoo <jasonyoo3026@gmail.com>
2023-05-04 21:42:59 +1000

    further restructuring in src folder

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+export { default as AuthBtn } from './Auth/AuthBtn';
+export { default as NewPost } from './Post/NewPost';
+export { default as RemoveBtn } from './Btn/RemoveBtn';
+export { default as SearchResults } from './SearchResults';
+export { default as PostLikes } from './Post/PostLikes';
+export { default as SignInBtn } from './Btn/SignInBtn';
+export { default as SignOutBtn } from './Btn/SignOutBtn';
+export { default as PostFeed } from './Post/PostFeed';
+export { default as RegisterBtn } from './Btn/RegisterBtn';
+export { default as SinglePost } from './Post/SinglePost';
+
